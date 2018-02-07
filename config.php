10f1918192b48064af89ce42c29ff29a523b718f
Mukarram <mukarram@Mukarrams-MacBook-Pro.local>
2018-02-07 18:28:28 +0530

    db config updated

--- config.php
+++ config.php
@@ -1,18 +1,11 @@
 <?php
+  $url = parse_url("mysql://b6df5ce52bbc63:f82ebc5b@us-cdbr-iron-east-05.cleardb.net/heroku_ec7331b11c15f13?reconnect=true");
+  $server = $url["host"];
+  $username = $url["user"];
+  $password = $url["pass"];
+  $db = substr($url["path"], 1);
 
-
-  $dbhost = "localhost";
-  $dbuser = "mukku";
-  $dbpass = "9917779786";
-  $dbname = "freesteps";
-
-
-  // Create connection
-  $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
-  // Check connection
-  if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-  }
+  $conn = new mysqli($server, $username, $password, $db);
 
   define("VEHICLE_TABLE", "vehicles");
   define("VEHICLE_ID", "vehicle_id");
@@ -21,11 +14,27 @@
   define("BEARINGS", "bearings");
   define("PHONE", "phone");
 
+  $query = "SELECT * FROM ".VEHICLE_TABLE;
+  $result = mysqli_query($conn, $query);
+
+  if(empty($result)) {
+    $query = "CREATE TABLE ".VEHICLE_TABLE." (
+              ".ID." int(11) AUTO_INCREMENT,
+              ".VEHICLE_ID." varchar(255) NOT NULL,
+              ".LATITUDE." varchar(255) NOT NULL,
+              ".LONGITUDE." int,
+              ".BEARINGS." int,
+              ".PHONE." int,
+              ".PRIMARY." KEY  (ID)
+              )";
+    $result = mysqli_query($conn, $query);
+    echo "Table created!";
+  }
+
   function failed(){
     die("{\"status\": false}");
   }
 
    //Table structure:
    //vehicles(id int, vehicle_id varchar(20), phone varchar(10), latitude decimal, longitude decimal, bearings decimal)
-
-   ?>
+?>
